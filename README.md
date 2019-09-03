c3a43de53915973504d97ce725fa2ca4dc2a75a2
Ashish Chaudhary <iamashishchaudhary@gmail.com>
2019-09-03 12:25:31 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ThreeJS Freeform Controls
---
+++ rollup.config.ts
@@ -0,0 +1,29 @@
+import resolve from "rollup-plugin-node-resolve"
+import commonjs from 'rollup-plugin-commonjs'
+import sourceMaps from 'rollup-plugin-sourcemaps'
+import camelCase from 'lodash.camelcase'
+import typescript from 'rollup-plugin-typescript2'
+import json from 'rollup-plugin-json'
+
+const pkg = require('./package.json')
+
+const libraryName = 'three-freeform-controls'
+
+export default {
+  input: `src/index.ts`,
+  output: [
+    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
+    { file: pkg.module, format: 'es', sourcemap: true }
+  ],
+  external: [],
+  watch: {
+    include: 'src/**'
+  },
+  plugins: [
+    json(),
+    typescript({ useTsconfigDeclarationDir: true }),
+    commonjs(),
+    resolve(),
+    sourceMaps()
+  ]
+}
