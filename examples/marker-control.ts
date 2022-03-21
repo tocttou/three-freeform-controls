35cea7003a525d0dfa158faa7d576bf0eb6a4832
g.remigi <g.remigi@kineticsystem.org>
2022-03-21 15:41:04 +0100

    Ongoing work.

--- examples/marker-control.ts
+++ examples/marker-control.ts
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { Vector3 } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 import * as FreeformControls from "../dist/three-freeform-controls.js";
@@ -22,12 +23,10 @@ const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
 scene.add(directionalLight);
 
 const orbitControls = new OrbitControls(camera, renderer.domElement);
-camera.position.set(0, 2, 5);
+camera.position.set(0, 2, 12);
 const gridHelper = new THREE.GridHelper(20, 20, 0x3a3a3a, 0x3a3a3a);
 gridHelper.position.y -= 0.51;
 scene.add(gridHelper);
-// const box = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshNormalMaterial());
-// scene.add(box);
 
 const animate = () => {
   requestAnimationFrame(animate);
@@ -37,37 +36,36 @@ const animate = () => {
 
 animate();
 
-const marker1 = new Marker(1.4, 0.6, 0.2, 1.2);
-marker1.translateOnAxis(new THREE.Vector3(1, 0, 0), 0);
-scene.add(marker1);
+// Marker 1
 
-const marker2 = new Marker(0.6, 0.6, 0.2, 2.4);
-marker2.translateOnAxis(new THREE.Vector3(1, 0, 0), 5);
-marker2.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 4);
-marker2.rotateOnAxis(new THREE.Vector3(0, 1, 0), Math.PI / 4);
-marker2.rotateOnAxis(new THREE.Vector3(0, 0, 1), Math.PI / 4);
-scene.add(marker2);
+const box1 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshNormalMaterial());
+box1.translateOnAxis(new Vector3(1, 0, 0), 4);
+scene.add(box1);
 
-const marker3 = new Marker(1, 0.6, 0.2, 0.6);
-marker3.translateOnAxis(new THREE.Vector3(1, 0, 0), -5);
-marker3.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 2);
-marker3.rotateOnAxis(new THREE.Vector3(0, 1, 0), Math.PI / 3);
-marker3.rotateOnAxis(new THREE.Vector3(0, 0, 1), Math.PI / 3);
-scene.add(marker3);
-
-// three-freeform-controls
-
-// initialize three-freeform-controls
-const controlsManager = new FreeformControls.ControlsManager(camera, renderer.domElement);
-//scene.add(controlsManager);
-
-// anchor controls to the box
-//const controls = controlsManager.anchor(box, {});
-
-// disable orbit controls while the freeform-controls are in use
-controlsManager.listen(FreeformControls.EVENTS.DRAG_START, () => {
+const marker1 = new Marker(camera, renderer.domElement, 1.4, 0.6, 0.2, 1.2);
+marker1.link(box1);
+marker1.listen(FreeformControls.EVENTS.DRAG_START, () => {
   orbitControls.enabled = false;
 });
-controlsManager.listen(FreeformControls.EVENTS.DRAG_STOP, () => {
+marker1.listen(FreeformControls.EVENTS.DRAG_STOP, () => {
   orbitControls.enabled = true;
 });
+
+scene.add(marker1);
+
+// Marker 2
+
+const box2 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshNormalMaterial());
+box2.translateOnAxis(new Vector3(1, 0, 0), -4);
+scene.add(box2);
+
+const marker2 = new Marker(camera, renderer.domElement, 0.7, 0.6, 0.2, 2);
+marker2.link(box2);
+marker1.listen(FreeformControls.EVENTS.DRAG_START, () => {
+  orbitControls.enabled = false;
+});
+marker2.listen(FreeformControls.EVENTS.DRAG_STOP, () => {
+  orbitControls.enabled = true;
+});
+
+scene.add(marker2);
--- examples/marker.ts
+++ examples/marker.ts
@@ -1,165 +1,313 @@
 import * as THREE from "three";
+import { Mesh } from "three";
+import * as FreeformControls from "../dist/three-freeform-controls";
 
-interface IRingOptions {
-  color?: number;
-}
-
-export class Ring extends THREE.Mesh {
-  private color: number;
-
-  constructor(private minRadius: number, private maxRadius: number, options?: IRingOptions) {
-    super();
-
-    this.color = options?.color ?? 0xffffff;
-    this.geometry = new THREE.BufferGeometry();
-
-    const sectors = 40;
-
-    // Create vertices.
-    const positions = [];
-    for (let i = 0; i < sectors; i++) {
-      const vertex1 = [
-        0,
-        this.minRadius * Math.cos((2 * Math.PI * i) / sectors),
-        this.minRadius * Math.sin((2 * Math.PI * i) / sectors),
-      ];
-      const vertex2 = [
-        0,
-        this.maxRadius * Math.cos((2 * Math.PI * i) / sectors),
-        this.maxRadius * Math.sin((2 * Math.PI * i) / sectors),
-      ];
-      positions.push(...vertex1);
-      positions.push(...vertex2);
-    }
-
-    this.geometry.setAttribute(
-      "position",
-      new THREE.BufferAttribute(new Float32Array(positions), 3)
-    );
-
-    // Create faces.
-    const indexes = [];
-    for (let i = 0; i < sectors; i++) {
-      const index1 = [
-        (2 * i) % (2 * sectors),
-        (2 * i + 1) % (2 * sectors),
-        (2 * i + 3) % (2 * sectors),
-      ];
-      const index2 = [
-        (2 * i) % (2 * sectors),
-        (2 * i + 3) % (2 * sectors),
-        (2 * i + 2) % (2 * sectors),
-      ];
-      indexes.push(...index1);
-      indexes.push(...index2);
-    }
-
-    this.geometry.setIndex(indexes);
-    this.geometry.computeVertexNormals();
-    this.geometry.clearGroups();
-
-    const pattern = [0, 1, 1, 0];
-    for (let i = 0; i < 2 * sectors; i++) {
-      this.geometry.addGroup(3 * i, 3, pattern[i % 4]);
-    }
-
-    // Assign materials.
-    const material1 = new THREE.MeshStandardMaterial({
-      color: this.color,
-      side: THREE.DoubleSide,
-    });
-    const material2 = new THREE.MeshStandardMaterial({
-      color: this.color,
-      opacity: 0.5,
-      transparent: true,
-      side: THREE.DoubleSide,
-    });
-
-    this.material = [material1, material2];
-  }
-}
-
-interface IArrowOptions {
-  color?: number;
-}
-
-export class Arrow extends THREE.Group {
-  private color: number;
-
-  constructor(private radius: number, private length: number, options?: IArrowOptions) {
-    super();
-
-    this.color = options?.color ?? 0xffffff;
-
-    const radialSegments = 32;
-    const arrowScale = 2;
-
-    const cylinderGeometry = new THREE.CylinderGeometry(radius, radius, length, radialSegments);
-    const cylimnderMaterial = new THREE.MeshStandardMaterial({ color: this.color });
-    const cylinder = new THREE.Mesh(cylinderGeometry, cylimnderMaterial);
-    this.add(cylinder);
-
-    const coneHeight = arrowScale * radius;
-
-    const coneGeometry = new THREE.ConeGeometry(coneHeight, coneHeight, radialSegments);
-    const coneMaterial = new THREE.MeshStandardMaterial({ color: this.color });
-
-    const cone = new THREE.Mesh(coneGeometry, coneMaterial);
-    cone.translateOnAxis(new THREE.Vector3(0, 1, 0), length / 2 + coneHeight / 2);
-    this.add(cone);
-  }
-}
-
-export class Marker extends THREE.Group {
+export class Marker extends FreeformControls.ControlsManager {
   constructor(
+    camera: THREE.Camera,
+    domElement: HTMLElement,
     private minRingRadius: number,
     private ringSize: number,
     private arrowRadius: number,
     private arrowLength: number
   ) {
+    super(camera, domElement);
+  }
+
+  public link = (object: THREE.Object3D): THREE.Group => {
+    const controls = this.anchor(object, {
+      hideOtherHandlesOnDrag: true,
+      hideOtherControlsInstancesOnDrag: false,
+      highlightAxis: false,
+      mode: FreeformControls.ANCHOR_MODE.INHERIT,
+    });
+    controls.showAll(false);
+    controls.setupHandle(new XTranslation(this.minRingRadius, this.arrowRadius, this.arrowLength));
+    controls.setupHandle(new YTranslation(this.minRingRadius, this.arrowRadius, this.arrowLength));
+    controls.setupHandle(new ZTranslation(this.minRingRadius, this.arrowRadius, this.arrowLength));
+    controls.setupHandle(new XRotation(this.minRingRadius, this.ringSize));
+    controls.setupHandle(new YRotation(this.minRingRadius, this.ringSize));
+    controls.setupHandle(new ZRotation(this.minRingRadius, this.ringSize));
+    return controls;
+  };
+}
+
+class Ring extends THREE.Mesh {
+  constructor(private minRadius: number, private maxRadius: number, private color: number) {
     super();
 
-    const maxRingRadius = minRingRadius + ringSize;
+    const sectors = 40;
+    this.geometry = new THREE.RingBufferGeometry(minRadius, maxRadius, sectors);
 
-    const xRing = new Ring(minRingRadius, maxRingRadius, { color: 0xff0000 });
-    this.add(xRing);
+    // Assing an index to each face, either 0 or 1, used to select a materials.
+    const pattern = [0, 1, 1, 0];
+    for (let i = 0; i < 2 * sectors; i++) {
+      this.geometry.addGroup(3 * i, 3, pattern[i % 4]);
+    }
 
-    const yRing = new Ring(minRingRadius, maxRingRadius, { color: 0x00ff00 });
-    yRing.rotateOnAxis(new THREE.Vector3(0, 0, 1), Math.PI / 2);
-    this.add(yRing);
-
-    const zRing = new Ring(minRingRadius, maxRingRadius, { color: 0x0000ff });
-    zRing.rotateOnAxis(new THREE.Vector3(0, 1, 0), Math.PI / 2);
-    this.add(zRing);
-
-    const xAxisPositive = new Arrow(arrowRadius, arrowLength, { color: 0xff0000 });
-    xAxisPositive.rotateOnAxis(new THREE.Vector3(0, 0, 1), -Math.PI / 2);
-    xAxisPositive.translateOnAxis(new THREE.Vector3(0, 1, 0), minRingRadius + arrowLength / 2);
-    this.add(xAxisPositive);
-
-    const xAxisNegative = new Arrow(arrowRadius, arrowLength, { color: 0xff0000 });
-    xAxisNegative.rotateOnAxis(new THREE.Vector3(0, 0, 1), Math.PI / 2);
-    xAxisNegative.translateOnAxis(new THREE.Vector3(0, 1, 0), minRingRadius + arrowLength / 2);
-    this.add(xAxisNegative);
-
-    const yAxisPositive = new Arrow(arrowRadius, arrowLength, { color: 0x00ff00 });
-    yAxisPositive.rotateOnAxis(new THREE.Vector3(1, 0, 0), 0);
-    yAxisPositive.translateOnAxis(new THREE.Vector3(0, 1, 0), minRingRadius + arrowLength / 2);
-    this.add(yAxisPositive);
-
-    const yAxisNegative = new Arrow(arrowRadius, arrowLength, { color: 0x00ff00 });
-    yAxisNegative.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI);
-    yAxisNegative.translateOnAxis(new THREE.Vector3(0, 1, 0), minRingRadius + arrowLength / 2);
-    this.add(yAxisNegative);
-
-    const zAxisPositive = new Arrow(arrowRadius, arrowLength, { color: 0x0000ff });
-    zAxisPositive.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 2);
-    zAxisPositive.translateOnAxis(new THREE.Vector3(0, 1, 0), minRingRadius + arrowLength / 2);
-    this.add(zAxisPositive);
-
-    const zAxisNegative = new Arrow(arrowRadius, arrowLength, { color: 0x0000ff });
-    zAxisNegative.rotateOnAxis(new THREE.Vector3(1, 0, 0), -Math.PI / 2);
-    zAxisNegative.translateOnAxis(new THREE.Vector3(0, 1, 0), minRingRadius + arrowLength / 2);
-    this.add(zAxisNegative);
+    // Create the materials.
+    this.material = [
+      new THREE.MeshStandardMaterial({
+        color: this.color,
+        side: THREE.DoubleSide,
+      }),
+      new THREE.MeshStandardMaterial({
+        color: this.color,
+        opacity: 0.5,
+        transparent: true,
+        side: THREE.DoubleSide,
+      }),
+    ];
   }
+}
+
+/**
+ * Handler to rotate the marker around the X axis.
+ */
+class XRotation extends FreeformControls.RotationGroup {
+  private ring: Ring;
+  constructor(private minRingRadius: number, private ringSize: number) {
+    super();
+    this.up = new THREE.Vector3(1, 0, 0);
+    this.ring = new Ring(minRingRadius, minRingRadius + ringSize, 0xff0000);
+    this.ring.rotateOnAxis(new THREE.Vector3(0, 1, 0), Math.PI / 2);
+    this.add(this.ring);
+  }
+
+  getInteractiveObjects = (): THREE.Object3D[] => {
+    return [this.ring];
+  };
+}
+
+/**
+ * Handler to rotate the marker around the Y axis.
+ */
+class YRotation extends FreeformControls.RotationGroup {
+  private ring: Ring;
+  constructor(private minRingRadius: number, private ringSize: number) {
+    super();
+    this.up = new THREE.Vector3(0, 1, 0);
+    this.ring = new Ring(minRingRadius, minRingRadius + ringSize, 0x00ff00);
+    this.ring.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 2);
+    this.add(this.ring);
+  }
+
+  getInteractiveObjects = (): THREE.Object3D[] => {
+    return [this.ring];
+  };
+}
+
+/**
+ * Handler to rotate the marker around the Z axis.
+ */
+class ZRotation extends FreeformControls.RotationGroup {
+  private ring: Ring;
+  constructor(private minRingRadius: number, private ringSize: number) {
+    super();
+    this.up = new THREE.Vector3(0, 0, 1);
+    this.ring = new Ring(minRingRadius, minRingRadius + ringSize, 0x0000ff);
+    this.add(this.ring);
+  }
+
+  getInteractiveObjects = (): THREE.Object3D[] => {
+    return [this.ring];
+  };
+}
+
+/**
+ * Handler to translate the marker along the X axis.
+ */
+class XTranslation extends FreeformControls.TranslationGroup {
+  parallel: THREE.Vector3;
+
+  private meshes: THREE.Mesh[];
+
+  constructor(
+    private minRingRadius: number,
+    private arrowRadius: number,
+    private arrowLength: number
+  ) {
+    super();
+
+    const radialSegments = 32;
+    const material = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+
+    this.meshes = [];
+    this.meshes.push(
+      new Mesh(
+        new THREE.CylinderGeometry(arrowRadius, arrowRadius, arrowLength, radialSegments)
+          .rotateZ(Math.PI / 2)
+          .translate(minRingRadius + arrowLength / 2, 0, 0),
+        material
+      )
+    );
+    this.meshes.push(
+      new Mesh(
+        new THREE.CylinderGeometry(arrowRadius, arrowRadius, arrowLength, radialSegments)
+          .rotateZ(Math.PI / 2)
+          .translate(-minRingRadius - arrowLength / 2, 0, 0),
+        material
+      )
+    );
+    this.meshes.push(
+      new Mesh(
+        new THREE.ConeGeometry(2 * arrowRadius, 2 * arrowRadius, radialSegments)
+          .rotateZ(-Math.PI / 2)
+          .translate(minRingRadius + arrowLength + arrowRadius, 0, 0),
+        material
+      )
+    );
+    this.meshes.push(
+      new Mesh(
+        new THREE.ConeGeometry(2 * arrowRadius, 2 * arrowRadius, radialSegments)
+          .rotateZ(Math.PI / 2)
+          .translate(-minRingRadius - arrowLength - arrowRadius, 0, 0),
+        material
+      )
+    );
+    this.meshes.map((mesh: Mesh) => {
+      this.add(mesh);
+    });
+
+    this.up = new THREE.Vector3(0, 1, 0);
+    this.parallel = new THREE.Vector3(1, 0, 0);
+  }
+
+  getInteractiveObjects = (): THREE.Object3D[] => {
+    return [...this.meshes];
+  };
+}
+
+/**
+ * Handler to translate the marker along the Y axis.
+ */
+class YTranslation extends FreeformControls.TranslationGroup {
+  parallel: THREE.Vector3;
+
+  private meshes: THREE.Mesh[];
+
+  constructor(
+    private minRingRadius: number,
+    private arrowRadius: number,
+    private arrowLength: number
+  ) {
+    super();
+
+    const radialSegments = 32;
+    const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+
+    this.meshes = [];
+    this.meshes.push(
+      new Mesh(
+        new THREE.CylinderGeometry(arrowRadius, arrowRadius, arrowLength, radialSegments).translate(
+          0,
+          minRingRadius + arrowLength / 2,
+          0
+        ),
+        material
+      )
+    );
+    this.meshes.push(
+      new Mesh(
+        new THREE.CylinderGeometry(arrowRadius, arrowRadius, arrowLength, radialSegments).translate(
+          0,
+          -minRingRadius - arrowLength / 2,
+          0
+        ),
+        material
+      )
+    );
+    this.meshes.push(
+      new Mesh(
+        new THREE.ConeGeometry(2 * arrowRadius, 2 * arrowRadius, radialSegments).translate(
+          0,
+          minRingRadius + arrowLength + arrowRadius,
+          0
+        ),
+        material
+      )
+    );
+    this.meshes.push(
+      new Mesh(
+        new THREE.ConeGeometry(2 * arrowRadius, 2 * arrowRadius, radialSegments)
+          .rotateX(-Math.PI)
+          .translate(0, -minRingRadius - arrowLength - arrowRadius, 0),
+        material
+      )
+    );
+    this.meshes.map((mesh: Mesh) => {
+      this.add(mesh);
+    });
+
+    this.up = new THREE.Vector3(0, 0, 1);
+    this.parallel = new THREE.Vector3(0, 1, 0);
+  }
+
+  getInteractiveObjects = (): THREE.Object3D[] => {
+    return [...this.meshes];
+  };
+}
+
+/**
+ * Handler to translate the marker along the Z axis.
+ */
+class ZTranslation extends FreeformControls.TranslationGroup {
+  parallel: THREE.Vector3;
+
+  private meshes: THREE.Mesh[];
+
+  constructor(
+    private minRingRadius: number,
+    private arrowRadius: number,
+    private arrowLength: number
+  ) {
+    super();
+
+    const radialSegments = 32;
+    const material = new THREE.MeshStandardMaterial({ color: 0x0000ff });
+
+    this.meshes = [];
+    this.meshes.push(
+      new Mesh(
+        new THREE.CylinderGeometry(arrowRadius, arrowRadius, arrowLength, radialSegments)
+          .rotateX(Math.PI / 2)
+          .translate(0, 0, minRingRadius + arrowLength / 2),
+        material
+      )
+    );
+    this.meshes.push(
+      new Mesh(
+        new THREE.CylinderGeometry(arrowRadius, arrowRadius, arrowLength, radialSegments)
+          .rotateX(Math.PI / 2)
+          .translate(0, 0, -minRingRadius - arrowLength / 2),
+        material
+      )
+    );
+    this.meshes.push(
+      new Mesh(
+        new THREE.ConeGeometry(2 * arrowRadius, 2 * arrowRadius, radialSegments)
+          .rotateX(Math.PI / 2)
+          .translate(0, 0, minRingRadius + arrowLength + arrowRadius),
+        material
+      )
+    );
+    this.meshes.push(
+      new Mesh(
+        new THREE.ConeGeometry(2 * arrowRadius, 2 * arrowRadius, radialSegments)
+          .rotateX(-Math.PI / 2)
+          .translate(0, 0, -minRingRadius - arrowLength - arrowRadius),
+        material
+      )
+    );
+    this.meshes.map((mesh: Mesh) => {
+      this.add(mesh);
+    });
+
+    this.up = new THREE.Vector3(1, 0, 0);
+    this.parallel = new THREE.Vector3(0, 0, 1);
+  }
+
+  getInteractiveObjects = (): THREE.Object3D[] => {
+    return [...this.meshes];
+  };
 }
